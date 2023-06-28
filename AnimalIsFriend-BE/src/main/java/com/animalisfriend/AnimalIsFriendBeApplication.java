96461d2591de548252d815bc213eb02c5ccd413b
태원 <103083087+yestw@users.noreply.github.com>
2023-06-28 16:17:51 +0900

    :tada: 백엔드 프로젝트 첫세팅

---
+++ AnimalIsFriend-BE/src/main/java/com/animalisfriend/AnimalIsFriendBeApplication.java
@@ -0,0 +1,13 @@
+package com.animalisfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalIsFriendBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalIsFriendBeApplication.class, args);
+	}
+
+}
---
+++ AnimalIsFriend-BE/src/test/java/com/animalisfriend/AnimalIsFriendBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalisfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalIsFriendBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
