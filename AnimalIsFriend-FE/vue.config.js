2111832b6668dbea1c0c7affd5bea66765784fca
태원 <103083087+yestw@users.noreply.github.com>
2023-07-10 15:00:41 +0900

    :sparkles: cors 설정 추가

--- AnimalIsFriend-FE/vue.config.js
+++ AnimalIsFriend-FE/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+require("dotenv").config();
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: process.env.VUE_APP_API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
+};
