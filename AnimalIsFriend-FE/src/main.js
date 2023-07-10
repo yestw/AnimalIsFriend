10eacabc47bd809dfb31c9f42bae7e170dd864bb
태원 <103083087+yestw@users.noreply.github.com>
2023-07-10 15:05:59 +0900

    :sparkles: 전역 토큰관리 구현
     - main.js 에서 전역 함수 만들어 토큰 관리

--- AnimalIsFriend-FE/src/main.js
+++ AnimalIsFriend-FE/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./routes";
+import VueCookies from "vue-cookies";
+import jwtDecode from "jwt-decode";
 
 Vue.config.productionTip = false
+Vue.use(VueCookies);
+Vue.$cookies.config("7d");
+
+Vue.prototype.$getTokenInfo = function (accessToken) {
+  if (!(accessToken == null)) {
+    const { sub, role } = jwtDecode(accessToken);
+    const tokenInfo = {
+      userId: sub,
+      role: role,
+    };
+    return tokenInfo;
+  }
+};
+
+Vue.prototype.$getAccessToken = function () {
+  const accessToken = this.$cookies.get("accessTokenCookie");
+  return accessToken;
+};
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
