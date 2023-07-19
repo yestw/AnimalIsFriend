39b295a7706ad71c1d977865bce953b1b394cdbe
태원 <103083087+yestw@users.noreply.github.com>
2023-07-19 15:28:56 +0900

    :sparkles: 구현 기능 라우터 추가

--- AnimalIsFriend-FE/src/routes/index.js
+++ AnimalIsFriend-FE/src/routes/index.js
@@ -3,6 +3,11 @@ import Router from "vue-router";
 import HomePage from "@/views/HomePage.vue";
 import LoginPage from "@/views/LoginPage.vue";
 import SignupPage from "@/views/SignupPage.vue";
+import PetRegisterPage from "@/views/PetRegisterPage.vue";
+import PetPage from "@/views/PetPage.vue";
+import PetDetailPage from "@/views/PetDetailPage.vue";
+import MessengerPage from "@/views/MessengerPage.vue";
+import ChatRoomPage from "@/views/ChatRoomPage.vue";
 
 Vue.use(Router);
 
@@ -23,6 +28,32 @@ export default new Router({
       path: "/signup",
       name: "signup",
       component: SignupPage,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: PetRegisterPage,
+    },
+    {
+      path: "/pet",
+      name: "pet",
+      component: PetPage,
+    },
+    {
+      path: "/pet/:pid",
+      name: "petDetail",
+      component: PetDetailPage,
+      props: true
+    },
+    {
+      path: "/messenger",
+      name: "messenger",
+      component: MessengerPage,
+    },
+    {
+      path: "/chatroom",
+      name: "chatroom",
+      component: ChatRoomPage,
     },
   ],
 });
