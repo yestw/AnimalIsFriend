5c31424781745b67c5f52b1a9e7eeb0eac1b4062
태원 <103083087+yestw@users.noreply.github.com>
2023-07-19 17:38:38 +0900

    :heavy_plus_sign: 의존성 수정
     - build 시 에러 해결

--- AnimalIsFriend-BE/src/test/java/com/animalisfriend/AnimalIsFriendBeApplicationTests.java
+++ AnimalIsFriend-BE/src/test/java/com/animalisfriend/AnimalIsFriendBeApplicationTests.java
@@ -1,13 +1,8 @@
-package com.animalisfriend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AnimalIsFriendBeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// package com.animalisfriend;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class AnimalIsFriendBeApplicationTests {
+// }
