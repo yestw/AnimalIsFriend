490d3072b1b8ca4c1a7f0e395231c73f489b007b
태원 <103083087+yestw@users.noreply.github.com>
2023-10-19 17:06:22 +0900

    :sparkle: ImageFile 도메인 기능 추가

--- AnimalIsFriend-BE/src/main/java/com/animalisfriend/domain/Image/repository/ImageFileRepository.java
+++ AnimalIsFriend-BE/src/main/java/com/animalisfriend/domain/Image/repository/ImageFileRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.repository.query.Param;
 
 import com.animalisfriend.domain.Image.entity.ImageFile;
 
+import java.util.Optional;
+
 public interface ImageFileRepository extends JpaRepository<ImageFile, Long> {
 
 
 	@Query("select i from ImageFile i where i.pets.petId = :petId")
 	ImageFile findByPetId(@Param("petId") Long petId);
+
+
+	@Query("select i from ImageFile  i where i.users.id = :userId")
+	Optional<ImageFile> findByUserId(@Param("userId") Long userId);
 }
